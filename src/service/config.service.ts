f984a478af75244d7fde2b3ee3541f9548b4d923
Jonas Claes <jonas@jonasclaes.be>
2025-12-26 22:20:10 +0100

    feat: load configuration values from environment variables via dotenv
    - Updated `ConfigService` to retrieve `OPCUA_URL`, `OPCUA_ROOT_PATH`, and `MQTT_URL` from environment variables.
    - Added default fallback values for these configurations.

--- src/service/config.service.ts
+++ src/service/config.service.ts
@@ -1,16 +1,19 @@
+import "dotenv/config";
+
 export type ConfigService = Awaited<ReturnType<typeof createConfigService>>;
 
 export const createConfigService = async () => {
-  const getOpcuaUrl = () => {
-    return "opc.tcp://192.168.30.4:4840";
-  };
-  const getOpcuaRootPath = () => {
-    return `ns=3;s="SmartHome_Data"`;
-  };
+    const getOpcuaUrl = () => {
+        return process.env.OMH_OPCUA_URL ?? "opc.tcp://192.168.30.4:4840";
+    };
 
-  const getMqttUrl = () => {
-    return "mqtt://localhost:1883";
-  };
+    const getOpcuaRootPath = () => {
+        return process.env.OMH_OPCUA_ROOT_PATH ?? `ns=3;s="SmartHome_Data"`;
+    };
 
-  return { getOpcuaUrl, getOpcuaRootPath, getMqttUrl };
+    const getMqttUrl = () => {
+        return process.env.OMH_MQTT_URL ?? "mqtt://localhost:1883";
+    };
+
+    return {getOpcuaUrl, getOpcuaRootPath, getMqttUrl};
 };
